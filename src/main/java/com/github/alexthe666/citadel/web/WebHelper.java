59ab67f2bc6b2214aa69a5bf5e72937fb4061579
WolfShotz <32242079+WolfShotz@users.noreply.github.com>
2019-09-08 18:18:18 -0400

    Fix a crash on loading URL contents when offline

--- src/main/java/com/github/alexthe666/citadel/web/WebHelper.java
+++ src/main/java/com/github/alexthe666/citadel/web/WebHelper.java
@@ -1,49 +1,36 @@
 package com.github.alexthe666.citadel.web;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.util.ResourceLocation;
-
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.io.*;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
 public class WebHelper {
 
     @Nullable
-    public static BufferedReader getURLContents(String urlString, String backupFileLoc){
-        BufferedReader reader = null;
-        boolean useBackup = false;
-        URL url;
+    public static BufferedReader getURLContents(@Nonnull String urlString, @Nonnull String backupFileLoc){
         try {
-            url = new URL(urlString);
-        } catch (MalformedURLException e) {
-            url = null;
-            useBackup = true;
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            InputStream stream = connection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(stream);
+            
+            return new BufferedReader(reader);
+        } catch (Exception e) { // Malformed URL, Offline, etc.
+            e.printStackTrace();
         }
-        if(url != null){
-            URLConnection connection = null;
-            try {
-                connection = url.openConnection();
-                InputStream is = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(is));
-            } catch (IOException e) {
-                e.printStackTrace();
-                useBackup = true;
-            }
+        
+        try { // Backup
+            return new BufferedReader(new InputStreamReader(WebHelper.class.getClass().getClassLoader().getResourceAsStream(backupFileLoc), StandardCharsets.UTF_8));
+        } catch (NullPointerException e) { // Can't parse backupFileLoc
+            e.printStackTrace();
         }
-        if(useBackup){
-            if (WebHelper.class.getClassLoader().getResourceAsStream(backupFileLoc) == null) {
-                backupFileLoc = "assets/citadel/backup_text.txt";
-            }
-            try {
-                reader = new BufferedReader(new InputStreamReader(WebHelper.class.getClass().getClassLoader().getResourceAsStream(backupFileLoc), "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        }
-        return reader;
+        
+        return null;
     }
 
 }
