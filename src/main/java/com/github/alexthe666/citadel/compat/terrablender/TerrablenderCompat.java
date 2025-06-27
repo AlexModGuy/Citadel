a593f5968ef475edd5f675fed23d368f9dbbf250
Alexthe668 <alex.rowlands@cox.net>
2025-06-27 10:38:39 -0700

    2.6.2

--- src/main/java/com/github/alexthe666/citadel/compat/terrablender/TerrablenderCompat.java
+++ src/main/java/com/github/alexthe666/citadel/compat/terrablender/TerrablenderCompat.java
@@ -3,7 +3,6 @@ package com.github.alexthe666.citadel.compat.terrablender;
 import com.github.alexthe666.citadel.Citadel;
 import com.github.alexthe666.citadel.server.generation.SurfaceRulesManager;
 import net.minecraft.world.level.levelgen.SurfaceRules;
-import terrablender.api.SurfaceRuleManager;
 
 import java.util.Map;
 
@@ -12,7 +11,7 @@ public class TerrablenderCompat {
     public static void setup(){
         Map<String, SurfaceRules.RuleSource> vanillaBiomeRules = SurfaceRulesManager.getOverworldRulesByBiomeForTerrablender(true);
         for(Map.Entry<String, SurfaceRules.RuleSource> entry : vanillaBiomeRules.entrySet()){
-            terrablender.api.SurfaceRuleManager.addToDefaultSurfaceRulesAtStage(terrablender.api.SurfaceRuleManager.RuleCategory.OVERWORLD, SurfaceRuleManager.RuleStage.BEFORE_BEDROCK, 0, entry.getValue());
+            terrablender.api.SurfaceRuleManager.addToDefaultSurfaceRulesAtStage(terrablender.api.SurfaceRuleManager.RuleCategory.OVERWORLD, terrablender.api.SurfaceRuleManager.RuleStage.BEFORE_BEDROCK, 0, entry.getValue());
         }
         Citadel.LOGGER.info("Added {} vanilla biome surface rule types via terrablender", vanillaBiomeRules.size());
         Map<String, SurfaceRules.RuleSource> moddedBiomeRules = SurfaceRulesManager.getOverworldRulesByBiomeForTerrablender(false);
