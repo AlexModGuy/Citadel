de065fa3b757a2581bd89b5f639d6db3af86c928
Alex Lee <islicy@outlook.com>
2026-01-02 22:02:28 +0800

    Update biome entries in CitadelBiomeDefinitions for consistency and accuracy

--- src/main/java/com/github/alexthe666/citadel/config/biome/CitadelBiomeDefinitions.java
+++ src/main/java/com/github/alexthe666/citadel/config/biome/CitadelBiomeDefinitions.java
@@ -3,8 +3,8 @@ package com.github.alexthe666.citadel.config.biome;
 public class CitadelBiomeDefinitions {
 
     public static final SpawnBiomeData TEST = new SpawnBiomeData()
-            .addBiomeEntry(BiomeEntryType.BIOME_DICT, false, "overworld", 0)
-            .addBiomeEntry(BiomeEntryType.BIOME_DICT, true, "water", 0)
+            .addBiomeEntry(BiomeEntryType.BIOME_TAG, false, "minecraft:is_overworld", 0)
+            .addBiomeEntry(BiomeEntryType.BIOME_TAG, true, "minecraft:is_ocean", 0)
             .addBiomeEntry(BiomeEntryType.REGISTRY_NAME, false, "minecraft:nether_wastes", 1);
 
     public static final SpawnBiomeData TERRALITH_TEST = new SpawnBiomeData()
