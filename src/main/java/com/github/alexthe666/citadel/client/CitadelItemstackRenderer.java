cc76490caf9253efb1179291bef4d35b7a1202bf
Alex <islicy@outlook.com>
2026-01-06 00:40:45 +0900

    Refactor CitadelItemstackRenderer to utilize DataComponents for item display properties

--- src/main/java/com/github/alexthe666/citadel/client/CitadelItemstackRenderer.java
+++ src/main/java/com/github/alexthe666/citadel/client/CitadelItemstackRenderer.java
@@ -3,6 +3,7 @@ package com.github.alexthe666.citadel.client;
 import com.github.alexthe666.citadel.Citadel;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Axis;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
@@ -11,12 +12,17 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.MobEffectTextureManager;
 import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.CustomData;
 import org.joml.Matrix4f;
 
 import java.util.HashMap;
@@ -44,14 +50,16 @@ public class CitadelItemstackRenderer extends BlockEntityWithoutLevelRenderer {
             Random random = new Random();
             boolean animateAnyways = false;
             ItemStack toRender = null;
-            //TODO: convert to component system
-            /*
-            if (stack.getTag() != null && stack.getTag().contains("DisplayItem")) {
-                String displayID = stack.getTag().getString("DisplayItem");
-                toRender = new ItemStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.parse(displayID)));
-                if (stack.getTag().contains("DisplayItemNBT")) {
+            // Use DataComponents system for 1.21+
+            CustomData customData = stack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY);
+            CompoundTag tag = customData.copyTag();
+            if (tag.contains("DisplayItem")) {
+                String displayID = tag.getString("DisplayItem");
+                toRender = new ItemStack(BuiltInRegistries.ITEM.get(ResourceLocation.parse(displayID)));
+                if (tag.contains("DisplayItemNBT")) {
                     try {
-                        toRender.setTag(stack.getTag().getCompound("DisplayItemNBT"));
+                        CompoundTag displayNBT = tag.getCompound("DisplayItemNBT");
+                        toRender.set(DataComponents.CUSTOM_DATA, CustomData.of(displayNBT));
                     } catch (Exception e) {
                         toRender = new ItemStack(Items.BARRIER);
                     }
@@ -63,23 +71,23 @@ public class CitadelItemstackRenderer extends BlockEntityWithoutLevelRenderer {
             }
             poseStack.pushPose();
             poseStack.translate(0.5F, 0.5f, 0.5f);
-            if(stack.getTag() != null && stack.getTag().contains("DisplayShake") && stack.getTag().getBoolean("DisplayShake")) {
+            if (tag.contains("DisplayShake") && tag.getBoolean("DisplayShake")) {
                 poseStack.translate((random.nextFloat() - 0.5F) * 0.1F, (random.nextFloat() - 0.5F) * 0.1F, (random.nextFloat() - 0.5F) * 0.1F);
             }
-            if(animateAnyways || stack.getTag() != null && stack.getTag().contains("DisplayBob") && stack.getTag().getBoolean("DisplayBob")){
+            if (animateAnyways || tag.contains("DisplayBob") && tag.getBoolean("DisplayBob")) {
                 poseStack.translate(0, 0.05F + 0.1F * Mth.sin(0.3F * ticksExisted), 0);
             }
-            if(stack.getTag() != null && stack.getTag().contains("DisplaySpin") && stack.getTag().getBoolean("DisplaySpin")){
+            if (tag.contains("DisplaySpin") && tag.getBoolean("DisplaySpin")) {
                 poseStack.mulPose(Axis.YP.rotationDegrees(6 * ticksExisted));
             }
-            if(animateAnyways || stack.getTag() != null && stack.getTag().contains("DisplayZoom") && stack.getTag().getBoolean("DisplayZoom")) {
+            if (animateAnyways || tag.contains("DisplayZoom") && tag.getBoolean("DisplayZoom")) {
                 float scale = (float) (1F + 0.15F * (Math.sin(ticksExisted * 0.3F) + 1F));
                 poseStack.scale(scale, scale, scale);
             }
-            if(stack.getTag() != null && stack.getTag().contains("DisplayScale") && stack.getTag().getFloat("DisplayScale") != 1.0F){
-                float scale = stack.getTag().getFloat("DisplayScale");
+            if (tag.contains("DisplayScale") && tag.getFloat("DisplayScale") != 1.0F) {
+                float scale = tag.getFloat("DisplayScale");
                 poseStack.scale(scale, scale, scale);
-            }*/
+            }
             Minecraft.getInstance().getItemRenderer().renderStatic(toRender, displayContext, packedLight, packedOverlay, poseStack, buffer, null, id);
             poseStack.popPose();
         }
@@ -89,14 +97,27 @@ public class CitadelItemstackRenderer extends BlockEntityWithoutLevelRenderer {
             RenderSystem.disableCull();
            // RenderSystem.enableAlphaTest();
             RenderSystem.enableDepthTest();
-            Holder<MobEffect> effect;
-            //TODO: convert to component system
-            if (mobEffectList == null) {
-                mobEffectList = BuiltInRegistries.MOB_EFFECT.holders().toList();
+            Holder<MobEffect> effect = null;
+            // Use DataComponents system for 1.21+
+            CustomData customData = stack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY);
+            CompoundTag tag = customData.copyTag();
+            if (tag.contains("DisplayEffect")) {
+                String effectId = tag.getString("DisplayEffect");
+                ResourceLocation effectLocation = ResourceLocation.parse(effectId);
+                MobEffect mobEffect = BuiltInRegistries.MOB_EFFECT.get(effectLocation);
+                if (mobEffect != null) {
+                    effect = BuiltInRegistries.MOB_EFFECT.wrapAsHolder(mobEffect);
+                }
             }
-            int size = mobEffectList.size();
-            int time = (int) (Util.getMillis() / 500);
-            effect = mobEffectList.get(time % size);
+            if (effect == null) {
+                // Fallback: cycle through all effects
+                if (mobEffectList == null) {
+                    mobEffectList = BuiltInRegistries.MOB_EFFECT.holders().toList();
+                }
+                int size = mobEffectList.size();
+                int time = (int) (Util.getMillis() / 500);
+                effect = mobEffectList.get(time % size);
+            }
             if (effect == null) {
                 effect = MobEffects.MOVEMENT_SPEED.getDelegate();
             }
@@ -107,13 +128,13 @@ public class CitadelItemstackRenderer extends BlockEntityWithoutLevelRenderer {
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
             RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
             RenderSystem.setShaderTexture(0, sprite.atlasLocation());
-            BufferBuilder bufferbuilder = Tesselator.getInstance().begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
+            BufferBuilder bufferbuilder = Tesselator.getInstance().begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
             Matrix4f mx = poseStack.last().pose();
-            int br = 255;
-            bufferbuilder.addVertex(mx, (float) 1, (float) 1, (float) 0).setUv(sprite.getU1(), sprite.getV0()).setColor(br, br, br, 255).setLight(packedLight);
-            bufferbuilder.addVertex(mx, (float) 0, (float) 1, (float) 0).setUv(sprite.getU0(), sprite.getV0()).setColor(br, br, br, 255).setLight(packedLight);
-            bufferbuilder.addVertex(mx, (float) 0, (float) 0, (float) 0).setUv(sprite.getU0(), sprite.getV1()).setColor(br, br, br, 255).setLight(packedLight);
-            bufferbuilder.addVertex(mx, (float) 1, (float) 0, (float) 0).setUv(sprite.getU1(), sprite.getV1()).setColor(br, br, br, 255).setLight(packedLight);
+            bufferbuilder.addVertex(mx, (float) 1, (float) 1, (float) 0).setUv(sprite.getU1(), sprite.getV0());
+            bufferbuilder.addVertex(mx, (float) 0, (float) 1, (float) 0).setUv(sprite.getU0(), sprite.getV0());
+            bufferbuilder.addVertex(mx, (float) 0, (float) 0, (float) 0).setUv(sprite.getU0(), sprite.getV1());
+            bufferbuilder.addVertex(mx, (float) 1, (float) 0, (float) 0).setUv(sprite.getU1(), sprite.getV1());
+            BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
             poseStack.popPose();
         }
         if (stack.getItem() == Citadel.ICON_ITEM.get()) {
